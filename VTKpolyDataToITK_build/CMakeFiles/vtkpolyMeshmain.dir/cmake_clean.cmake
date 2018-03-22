fe8e00d8bb71cd22275bebf049014cb23f9f2f0b
sassystacks <rbeddoes@shaw.ca>
2018-03-22 01:58:08 -0600

    added visualization

---
+++ VTKpolyDataToITK_build/CMakeFiles/vtkpolyMeshmain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtkpolyMeshmain.dir/vtkpolyTomesh.cxx.o"
+  "vtkpolyMeshmain.pdb"
+  "vtkpolyMeshmain"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtkpolyMeshmain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
